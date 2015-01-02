6e4998dabd66c24bbb866a5124c92dcffed89d23
Fabian <fabianjul@gmail.com>
2015-01-02 14:35:00 +0100

    Fixed YCM compile flags, works now

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,33 +1,3 @@
-# This file is NOT licensed under the GPLv3, which is the license for the rest
-# of YouCompleteMe.
-#
-# Here's the license text for this file:
-#
-# This is free and unencumbered software released into the public domain.
-#
-# Anyone is free to copy, modify, publish, use, compile, sell, or
-# distribute this software, either in source code form or as a compiled
-# binary, for any purpose, commercial or non-commercial, and by any
-# means.
-#
-# In jurisdictions that recognize copyright laws, the author or authors
-# of this software dedicate any and all copyright interest in the
-# software to the public domain. We make this dedication for the benefit
-# of the public at large and to the detriment of our heirs and
-# successors. We intend this dedication to be an overt act of
-# relinquishment in perpetuity of all present and future rights to this
-# software under copyright law.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-# OTHER DEALINGS IN THE SOFTWARE.
-#
-# For more information, please refer to <http://unlicense.org/>
-
 # These are the compilation flags that will be used in case there's no
 # compilation database set (by default, one is not set).
 #
@@ -37,50 +7,29 @@
 # -I:
 #       add directory to include search path
 
-# These are the compilation flags that will be used in case there's no
-# compilation database set (by default, one is not set).
 flags = [
 '-Wall',
 '-Wextra',
 '-Werror',
 '-Wc++98-compat',
-'-Wno-long-long',
-'-Wno-variadic-macros',
-'-fexceptions',
 '-DNDEBUG',
-'-x', 'c++',
 '-std=c++11',
-'-stdlib=libc++',
-'-I', '/usr/include/c++/',
-'-L', '/usr/include/c++/',
-'-I', '.'
+'-x', 'c++',
+'-I', '.',
+'-isystem', '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1',
+'-isystem', '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include'
 ]
 
 # compile command
-def main():
-    print(' '.join(flags))
-
 if __name__ == '__main__':
     import sys
     if 'compile-flags' in sys.argv:
-        main()
+        print(' '.join(flags))
         sys.exit(0)
 
 import os
 import ycm_core
 
-
-
-# Set this to the absolute path to the folder (NOT the file!) containing the
-# compile_commands.json file to use that instead of 'flags'. See here for
-# more details: http://clang.llvm.org/docs/JSONCompilationDatabase.html
-#
-# You can get CMake to generate this file for you by adding:
-#   set( CMAKE_EXPORT_COMPILE_COMMANDS 1 )
-# to your CMakeLists.txt file.
-#
-# Most projects will NOT need to set this to anything; you can just change the
-# 'flags' list of compilation flags. Notice that YCM itself uses that approach.
 compilation_database_folder = ''
 
 if os.path.exists( compilation_database_folder ):
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,9 @@
+#include "tar.hpp"
+
+#include <string>
 
 int main() {
+        std::cout << "Hello, World!" << std::endl;
         return 0;
 }
 
